c47e7372aac995628a2a197f1ba14a577285b7a8
RachanaSonu <rachana.p1398@gmail.com>
2023-12-24 13:27:34 +0530

    app

--- gender_project/settings.py
+++ gender_project/settings.py
@@ -76,11 +76,22 @@ WSGI_APPLICATION = 'gender_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+	'default': {
+		'ENGINE': 'django.db.backends.mysql',
+		'NAME': 'gender',
+		'USER': 'root',
+		'PASSWORD': 'admin',
+		'HOST':'localhost',
+		'PORT':'3306',
+	}
 }
 
 
